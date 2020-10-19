31e408fa27db48a681b8a195e5edd9753b54a035
Toasterapp <s.toaster.app@gmail.com>
2020-10-20 04:45:35 +0900

    入力まわり

--- NecRougue/Assets/CafeAssets/Script/Installer/ProjectInstaller.cs
+++ NecRougue/Assets/CafeAssets/Script/Installer/ProjectInstaller.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using CafeAssets.Script.Sequence;
 using CafeAssets.Script.System.PropsSystem;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using Zenject;
 
 public class ProjectInstaller : MonoInstaller
 {
+    [SerializeField] private EventSystem _eventSystem;
     public override void InstallBindings()
     {
         Debug.Log("[PJIns] Install");
@@ -15,5 +17,7 @@ public class ProjectInstaller : MonoInstaller
         Container.Bind<RootSequence>().FromNewComponentOnNewGameObject().AsSingle().Lazy();
         //Props
         Container.Bind<Props>().FromNewComponentOnNewGameObject().AsSingle().NonLazy();
+        //
+        Container.Bind<EventSystem>().FromComponentInNewPrefab(_eventSystem).AsSingle().NonLazy();
     }
 }
--- NecRougue/Assets/CafeAssets/Script/System/GameMapSystem/MapView.cs
+++ NecRougue/Assets/CafeAssets/Script/System/GameMapSystem/MapView.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.Tilemaps;
 using NotImplementedException = System.NotImplementedException;
 
@@ -12,25 +13,65 @@ namespace CafeAssets.Script.System.GameMapSystem
         void SetTile(TileModel tile, Vector3Int pos);
         void RemoveTile(Vector3Int pos);
     }
-    public class MapView : MonoBehaviour,IMapView
+
+    public class MapView : MonoBehaviour, IMapView
     {
         [SerializeField] private Tilemap _tilemap;
         [SerializeField] private TilemapRenderer _tilemapRenderer;
+        private bool _placeTileMode = false;
+        
         public void Reset()
         {
             Debug.Log("[MapView]Reset");
             _tilemap.ClearAllTiles();
-           
+
         }
 
         public void SetTile(TileModel tile, Vector3Int pos)
         {
-            _tilemap.SetTile(pos,tile);
+            _tilemap.SetTile(pos, tile);
         }
 
         public void RemoveTile(Vector3Int pos)
         {
-            _tilemap.SetTile(pos,null);
+            _tilemap.SetTile(pos, null);
+        }
+
+        void PlaceTile(Vector2 pos)
+        {
+            
+        }
+
+        void MoveCamera(Vector2 move)
+        {
+            var cam = Camera.main;
+            cam.gameObject.transform.Translate(move);
+        }
+        public void OnPointerDown(BaseEventData e)
+        {
+            var pe = (PointerEventData) e;
+            if (_placeTileMode)
+            {
+                PlaceTile(pe.position);
+            }
+           
+
+        }
+
+        public void OnPointerUp()
+        {
+            
+        }
+
+        public void OnDrag(BaseEventData e)
+        {
+            var pe = (PointerEventData) e;
+            Debug.Log(pe.delta);
+            if (!_placeTileMode)
+            {
+                
+                MoveCamera(pe.delta);
+            }
         }
     }
 }
